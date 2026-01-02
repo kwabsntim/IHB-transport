a40da26404dee9e13347c9980a41da6cdeadc90c
kwabsntim <daffourkwabena@gmail.com>
2026-01-02 10:11:33 +0000

    Refactor admin account seeding: update environment variable names and improve error handling

--- cmd/seed/main.go
+++ cmd/seed/main.go
@@ -1,21 +1,56 @@
-package seed
+package main
 
-import(
-	"log"
-	"ihb-transport/internal/models"
-	"gorm.io/gorm"
+//this creates a default admin account if not created
+import (
 	"ihb-transport/internal/auth"
+	"ihb-transport/internal/database"
+	"ihb-transport/internal/models"
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
+	"gorm.io/gorm"
 )
 
+func seedAdmin(db *gorm.DB) {
 
-func DefaultAdminAccount(db *gorm.DB){
+	if err := godotenv.Load(); err != nil {
+		log.Println("Warning: .env file not found")
+	}
+
 	var count int64
 	db.Model(&models.Admin{}).Count(&count)
-	if count>0{
+	if count > 0 {
 		log.Println("Admin account exists, skipping seeding.")
 		return
 	}
-}
+	// Read from environment variable
+	password := os.Getenv("ADMIN_PASSWORD")
+	if password == "" {
+		log.Fatal("no admin password found")
+	}
+	email := os.Getenv("ADMIN_EMAIL")
+	if email == "" {
+		log.Fatal("no admin email found")
+	}
+	//hashing password
+	hashedPassword, err := auth.HashPassword(password)
+	if err != nil {
+		log.Fatalf("Failed to hash password: %v", err)
+		return
+	}
+	//creating admin account
+	admin := models.Admin{
+		Email:        email,
+		PasswordHash: hashedPassword,
+	}
+	db.Create(&admin)
 
-//hashing password 
-hashedPassword,err:=auth.
+}
+func main() {
+	godotenv.Load()
+	database.Connection()
+	db := database.DB
+
+	seedAdmin(db)
+}
